73bdbda4b84618d60dd86fb37c06d47d05355055
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-07-15 23:46:27 +0300

    adjusteed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 const registerSchema = z.object({
   email: z.string().email("Invalid email"),
   password: z.string().min(8, "Too short").max(50, "Too long"),
-  date: z.string(),
+  date: z.string().min(1, "Date is required"),
   agreed: z.boolean().refine((value) => value === true, {
     message: "You must agree to the terms and conditions",
   }),
